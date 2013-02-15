4e2aab6bada16436c2a2dbcda2236d07a8c3781a
Matt Phillips <mphillips@law.harvard.edu>
2013-02-15 14:29:34 -0500

    some display tweaks on the search page

--- batch/get_thumbnails.php
+++ batch/get_thumbnails.php
@@ -7,34 +7,36 @@ function thumbnail_from_url($slug, $url){
     global $master_config;
     //print "slug is $slug and url is $url\n";
 
-    exec('/Applications/wkhtmltopdf.app/Contents/MacOS/wkhtmltopdf --quiet ' . $url . ' ' . $master_config['LAW_APPS_HOME'] . '/web/images/db-thumbs/' . $slug . '.pdf');
-    exec('convert -quiet ' . $master_config['LAW_APPS_HOME'] . '/web/images/db-thumbs/' . $slug . '.pdf ' . $master_config['LAW_APPS_HOME'] . '/web/images/db-thumbs/' . $slug . '.jpg');
-    exec('convert -quiet ' . $master_config['LAW_APPS_HOME'] . '/web/images/db-thumbs/' . $slug . '.jpg -resize 250x250 ' . $master_config['LAW_APPS_HOME'] . '/web/images/db-thumbs/' . $slug . '_thumb.jpg');
+
+    // Some online resources have the same slug. If we've already created a thumb for that slug, don't do anything.
+    if (!file_exists($master_config['LAW_APPS_HOME'] . '/web/images/db-thumbs/' . $slug . '_thumb.jpg')) {
+        exec('/opt/wkhtmltopdf/bin/wkhtmltopdf --quiet -B 0 -L 0 -R 0 -T 0 ' . $url . ' ' . $master_config['LAW_APPS_HOME'] . '/web/images/db-thumbs/' . $slug . '.pdf');
+        exec('convert -quiet ' . $master_config['LAW_APPS_HOME'] . '/web/images/db-thumbs/' . $slug . '.pdf ' . $master_config['LAW_APPS_HOME'] . '/web/images/db-thumbs/' . $slug . '.jpg');
+        exec('convert -quiet ' . $master_config['LAW_APPS_HOME'] . '/web/images/db-thumbs/' . $slug . '.jpg -resize 250x250 ' . $master_config['LAW_APPS_HOME'] . '/web/images/db-thumbs/' . $slug . '_thumb.jpg');
     
-    if (!file_exists($master_config['LAW_APPS_HOME'] . '/law-apps/web/images/db-thumbs/' . $slug . '_thumb.jpg')) {
-        $message_to_file = date('l jS \of F Y h:i:s A') . ' -- Unable to create' . $slug . "_thumb.jpg for $url Creating a placeholder.\n";
+        if (!file_exists($master_config['LAW_APPS_HOME'] . '/web/images/db-thumbs/' . $slug . '_thumb.jpg')) {
+            $message_to_file = date('l jS \of F Y h:i:s A') . ' -- Unable to create' . $slug . "_thumb.jpg for $url Creating a placeholder.\n";
         
-        file_put_contents($master_config['LAW_APPS_HOME'] . '/log/thumbnail.log', $message_to_file);
-        exec('cp ' . $master_config['LAW_APPS_HOME'] . '/web/images/db-thumbs/blank.jpg ' . $master_config['LAW_APPS_HOME'] . '/web/images/db-thumbs/' . $slug . '_thumb.jpg');
-    }
+            file_put_contents($master_config['LAW_APPS_HOME'] . '/log/thumbnail.log', $message_to_file, FILE_APPEND);
+            exec('cp ' . $master_config['LAW_APPS_HOME'] . '/web/images/db-thumbs/blank.jpg ' . $master_config['LAW_APPS_HOME'] . '/web/images/db-thumbs/' . $slug . '_thumb.jpg');
+        }
 
-    // Do some cleanup. Remove our PDF
-    if (file_exists($master_config['LAW_APPS_HOME'] . '/law-apps/web/images/db-thumbs/' . $slug . '.pdf')) {
-        exec('rm ' . $master_config['LAW_APPS_HOME'] . '/law-apps/web/images/db-thumbs/' . $slug . '.pdf');
-    }
+        // Do some cleanup. Remove our PDF
+        if (file_exists($master_config['LAW_APPS_HOME'] . '/web/images/db-thumbs/' . $slug . '.pdf')) {
+            exec('rm ' . $master_config['LAW_APPS_HOME'] . '/web/images/db-thumbs/' . $slug . '.pdf');
+        }
 
-    // Do some cleanup. Remove our fullsize jpg    
-    if (file_exists($master_config['LAW_APPS_HOME'] . '/law-apps/web/images/db-thumbs/' . $slug . '.jpg')) {
-        exec('rm ' . $master_config['LAW_APPS_HOME'] . '/law-apps/web/images/db-thumbs/' . $slug . '.jpg');
+        // Do some cleanup. Remove our fullsize jpg    
+        if (file_exists($master_config['LAW_APPS_HOME'] . '/web/images/db-thumbs/' . $slug . '.jpg')) {
+            exec('rm ' . $master_config['LAW_APPS_HOME'] . '/web/images/db-thumbs/' . $slug . '.jpg');
+        }
     }
-
 }
 
-$es_url= 'http://localhost/law-apps/api/item/search?limit=20';
+$es_url= 'http://hlsl7.law.harvard.edu/dev/matt/law-apps/api/item/search?limit=10';
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $es_url);
-//curl_setopt($ch, CURLOPT_HEADER, true);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 $a = curl_exec($ch);
--- web/js/scan.js
+++ web/js/scan.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('#search-awesome').submit(function() {
 		var query = $("#query").val();
-		$.getJSON("/law-apps/api/item/search?callback=?&limit=45&filter=_all:" + query, function(data) {
+		$.getJSON("http://hlsl7.law.harvard.edu/dev/matt/law-apps/api/item/search?callback=?&limit=45&filter=_all:" + query, function(data) {
 		  showResults(data);
 		});
 		return false;
@@ -9,8 +9,7 @@ $(document).ready(function() {
 });
 
 function showResults(data){ 
-    console.log(data);
   var source = $("#search-template").html();
-	var template = Handlebars.compile(source);
+  var template = Handlebars.compile(source);
   $('#search-results').html(template(data));
 }
