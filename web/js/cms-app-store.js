620fb1a9ca1c80cffab781214893abb467586e33
Annie Cain <anniejocain@gmail.com>
2013-02-21 13:52:30 -0500

    facets and paging in cms script

--- web/js/cms-app-store.js
+++ web/js/cms-app-store.js
@@ -3,6 +3,15 @@
 
 //var $ = jQuery.noConflict();
 
+var params = {
+    limit: 25,
+    start: 0
+};
+
+var filters = [];
+
+var api_response = null;
+
 $(document).ready(function() {
   $('head').append('<link rel="stylesheet" href="http://hlsl10.law.harvard.edu/dev/annie/law-apps/css/cms-app-store.css" type="text/css" />');
   $('#search-template').hide();
@@ -10,34 +19,124 @@ $(document).ready(function() {
 
   $('#search-apps').submit(function() {
 		var query = $("#query").val();
-		$.getJSON("http://hlsl10.law.harvard.edu/dev/annie/law-apps/api/item/search?callback=?&limit=45&filter=_all:" + query, function(data) {
-		  //showResults(data);
-		  $('#search-results').html('<p>' + data.num_found + ' results</p>');
+		/*$.getJSON("http://hlsl10.law.harvard.edu/dev/annie/law-apps/api/item/search?callback=?&limit=45&filter[]=_all:" + query, function(data) {
+		  showResults(data);
+		});*/
+		params.start = 0;
+    getResults();
+		return false;
+	});
+
+	
+	$(".filter").live("click", function(event){
+    filters.push('category:' + $(this).attr("id"));
+    params.start = 0;
+    getResults();
+  });
+  
+  $("#clear").live("click", function(event){
+      filters.length = 0;
+      params.start = 0;
+      getResults();
+  });
+  
+  $("#prev").live("click", function(event){
+      if (params.start - params.limit >= 0) {
+          params.start = params.start - params.limit;
+      }
+      getResults();
+  });
+  
+  $("#next").live("click", function(event){
+      if (params.start + params.limit <= api_response.num_found) {
+          params.start = params.start + params.limit;
+      }
+      getResults();
+  });
+	
+});
+
+function getResults() {
+    var query = $("#query").val();
+
+    var api_url = "http://hlsl10.law.harvard.edu/dev/annie/law-apps/api/item/search?callback=?&sort=clicks desc&limit=" + params.limit + "&start=" + params.start;
+
+    $.each(filters, function(index, value) {
+        api_url = api_url + "&filter[]=" + value;
+    });
+    
+    if (query) {
+        api_url = api_url + "&filter[]=_all:" + query;
+    }
+
+    $.getJSON(api_url, function(json_data) {
+        api_response = json_data;
+        showResults();
+        showFacets();
+        showControls();
+    });
+}
+
+function showResults(){ 
+  $('#search-results').html('<p>' + api_response.num_found + ' results</p>');
 		  var results_list = '';
-		  $.each(data.docs, function(key, value) { 
+		  $.each(api_response.docs, function(key, value) { 
         results_list += '<div class="result"><h2><a href="' + value.link + '" target="_blank">' + value.name + '</a></h2>';
         results_list += '<a href="' + value.link + '"><span class="preview"><img src="http://hlsl10.law.harvard.edu/dev/annie/law-apps/images/db-thumbs/' + value.slug +'_thumb.jpg" alt=""></span></a>';
         results_list += '<span class="desc">' + value.description + '</span></div>';
       });
       $('#search-results').append(results_list);
-      //$('#search-results').append('<dl>' + results_list + '</dl>').fadeIn();
-		});
-		return false;
-	});
-	
-	$('.wysiwyg').on("click", "dd a, li a, dt a", function(event) {
-		var link = $(this).attr('href');
-		var url = "http://hlsl10.law.harvard.edu/dev/annie/law-apps/api/item/click";
-      
-    $.post(url, {link: link}, function(data) {
-      //$('#response').html('<p>clicked ' + link + '</p>');
-    });
-	});
-	
-});
+}
 
-function showResults(data){ 
-  var source = $("#search-template").html();
-	var template = Handlebars.compile(source);
-  $('#search-results').html(template(data));
+function showFacets(){ 
+  var filters_list = '';
+  $.each(api_response.facets.category.terms, function(key, value) { 
+    filters_list += '<li id="' + value.term + '" class="filter control-action">' + value.term + ' (' + value.count + ')</li>';
+	});
+	$('#filters').html('<ul>' + filters_list + '</ul>');
+}
+
+function showControls(){
+  var controls_html = '';
+    var data = {num_found: api_response.num_found};
+    data.start = params.start + 1;
+    
+    if (params.start + params.limit < api_response.num_found) {
+        data.end = params.start + params.limit;
+    } else {
+        data.end = api_response.num_found;
+    }
+    
+    if (data.end < api_response.num_found ) {
+        data.next = true;
+    }
+
+    if (data.start >= params.limit ) {
+        data.prev = true;
+    }
+    
+    if (filters.length !== 0) {
+        data.clear = true;
+    }
+    
+    if(data.clear) {
+			controls_html += '<p id="clear" class="control-action">Clear filters</p>';
+		}
+		
+		controls_html += '<p>Showing ' + data.start + ' to ' + data.end + ' of ' + data.num_found + ' results';
+
+		if(data.prev) {
+			controls_html += ' <span id="prev" class="control-action">Previous</span>';
+		}
+		
+		if(data.next) {
+			controls_html += ' <span id="next" class="control-action">Next</span>';
+		}
+		
+		controls_html += '</p>';
+		$('#controls').html(controls_html);
+    
+    //var source = $("#controls-template").html();
+    //var template = Handlebars.compile(source);
+    //$('#controls').html(template(data));
 }
